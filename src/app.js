ddbb88902e8c365528b3940f678b6b607afa8f5f
marchelo1 <marchelo1_@msn.com>
2020-01-04 01:14:52 +0100

    One Component per File

--- src/app.js
+++ src/app.js
@@ -1,51 +1,5 @@
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Indecision</h1>
-        <h2>Put your life in the hands of a computer</h2>
-      </div>
-    );
-  }
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-class Action extends React.Component {
-  render() {
-    return (
-      <div>
-        <button>What should I do?</button>
-      </div>
-    );
-  }
-}
-
-class Options extends React.Component {
-  render() {
-    return (
-      <div>
-        Options component here
-      </div>
-    );
-  }
-}
-
-class AddOption extends React.Component {
-  render() {
-    return (
-      <div>
-        AddOption component here
-      </div>
-    );
-  }
-}
-
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
